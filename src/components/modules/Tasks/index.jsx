f246a2bfd0467e34e9c19e1c744dac3c8432176d
perfection1st <107283558+perfection1st@users.noreply.github.com>
2023-03-04 11:54:33 -0500

    Adds time/day module

---
+++ src/components/modules/Tasks/index.jsx
@@ -0,0 +1,5 @@
+const Tasks = () => {
+  return <h1>Tasks module</h1>;
+};
+
+export default Tasks;
--- src/components/modules/Time/index.jsx
+++ src/components/modules/Time/index.jsx
@@ -1,10 +1,43 @@
 import './Time.css';
+import { useState, useEffect } from 'react';
 
 const Time = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function getCurrentTime(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    return `${hours}:${minutes}${ampm}`;
+  }
+
+  function getCurrentDay() {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    const formattedDate = date.toLocaleString('en-US', options);
+    return formattedDate;
+  }
+
   return (
     <div>
-      <h1 id="current-time">2:30PM</h1>
-      <p id="current-date">Sunday, February 12</p>
+      <h1 id="current-time">{getCurrentTime(time)}</h1>
+      <p id="current-date">{getCurrentDay()}</p>
     </div>
   );
 };
